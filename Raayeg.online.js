b3bd67ee3a0d5217812f412a0aba7570760a8803
Cheikh Oumou <67443473+cheikhoumou@users.noreply.github.com>
2023-02-15 04:32:23 -0800

    v1
    v1

---
+++ Raayeg.online.js
@@ -0,0 +1,71 @@
+const sheetId = '1JIpJgzg8nqDzEN96ENvyG_h5StHB6AEguwtCsPmvk4E';
+const base = `https://docs.google.com/spreadsheets/d/${sheetId}/gviz/tq?`;
+const sheetName = 'DATA';
+const query = encodeURIComponent('Select *')
+const url = `${base}&sheet=${sheetName}&tq=${query}`
+const RaayegDAta1 = []
+document.addEventListener('DOMContentLoaded', init)
+function init() {
+    fetch(url)
+        .then(res => res.text())
+        .then(rep => {
+            const jsonData = JSON.parse(rep.substring(47).slice(0, -2));
+            const colz = [];
+            jsonData.table.cols.forEach((heading) => {
+                if (heading.label) {
+                    let column = heading.label;
+                    colz.push(column);
+                }
+            })
+            jsonData.table.rows.forEach((rowData) => {
+                const row = {};
+                colz.forEach((ele, ind) => {
+                    row[ele] = (rowData.c[ind] != null) ? rowData.c[ind].v : '';
+                })
+                RaayegDAta1.push(row);
+            })
+        })
+}
+
+function LoadData(){
+    if (typeof(Storage) !== "undefined") {
+      document.getElementById("username").value=localStorage.getItem("userRaayeg");
+      document.getElementById("password").value =localStorage.getItem("passRaayeg");
+      document.getElementById("course").value =localStorage.getItem("course1");
+    }
+    if(username!="" && password!=""){
+      for(var i = 0; i < RaayegDAta1.length; i++)  {
+      if(RaayegDAta1[i].usarname == username ){return}
+      }
+      localStorage.clear();
+    }
+  }
+  
+  function LoginUser()  {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+      if(username!="" && password!=""){
+      for(var i = 0; i < RaayegDAta1.length; i++)  {
+      if(RaayegDAta1[i].usarname == username && 
+        RaayegDAta1[i].password == password){
+          if (typeof(Storage) !== "undefined") {
+            localStorage.setItem("userRaayeg", username);
+            localStorage.setItem("passRaayeg", password);
+            localStorage.setItem("course1", RaayegDAta1[i].course);
+          }
+          document.getElementById("displayusername").innerHTML = username;
+          document.getElementById("page1_id1").className = "page1_class1-off";
+          document.getElementById("page4_id1").className = "page4_id1";  
+          return
+        }else {
+        document.getElementById("errorMessage").innerHTML = " البيانات غير متطابقة اذا لم يكن لديك حساب تواصل معنا ";
+        localStorage.clear();
+        }
+      }
+    }    
+  }  
+   
+  function function3(){ 
+    document.getElementById("page3_id1").className = "page3_id1-off";
+    document.getElementById("page1_id1").className = "page1_id1"; 
+  }
