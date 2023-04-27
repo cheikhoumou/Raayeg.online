9a490d3a2be370a9dcfca1d06ab6082da7aeb0ea
Cheikh Oumou <67443473+cheikhoumou@users.noreply.github.com>
2023-04-26 19:08:17 -0700

    v2
    v1

--- raayegcripto2.js
+++ raayegcripto2.js
@@ -1,21 +1,21 @@
-function tchek(){
-        if(localStorage.getItem('userRaayegc1') == null || localStorage.getItem('passRaayegc1') == null || localStorage.getItem('course2') == null){
-        console.log('logout'); 
-        localStorage.clear();
-        document.querySelector('.loging').style = `display: flex;`;
-        document.querySelector('.user.claynt').style = `display: none !important;`; 
-    }else{
-        document.querySelector('.user.claynt').style = `display: block !important;`; 
-        document.querySelector('.loging').style = `display: none;`;
-        console.log('login');
-    }}tchek();
+function tchek2(){
+  if(localStorage.getItem('userRaayegc1') == null || localStorage.getItem('passRaayegc1') == null || localStorage.getItem('course2') == null){
+  console.log('logout'); 
+  localStorage.clear();
+  document.querySelector('.loging').style = `display: flex;`;
+  document.querySelector('.user.claynt').style = `display: none !important;`; 
+}else{
+  document.querySelector('.user.claynt').style = `display: block !important;`; 
+  document.querySelector('.loging').style = `display: none;`;
+  console.log('login');
+}}tchek2();
 document.querySelector('input.log').onclick = function(){
-    tchek();
+tchek2();
 document.querySelector('.listus p.nameus').innerHTML = localStorage.getItem('userRaayegc1');
 }
 document.querySelector('div#nav div#HTML6 .widget-content button').onclick = function(){
-    localStorage.clear();
-    window.location = 'https://raayegcrypto.blogspot.com/';
+localStorage.clear();
+window.location = 'https://raayegcrypto.blogspot.com/';
 }
 //code
 if (document.querySelectorAll('.date-posts-row .post-outer').length == 1){
